91e778cabaaa8c35f959ebd71c7003c204a8303d
NikolaKoltin <nikola.koltin@tcu.edu>
2025-11-14 15:10:00 -0600

    Completed question8

---
+++ question8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Global variable
+int globalVar = 10;
+
+int main() {
+    // Local variable
+    int localVar = 5;
+
+    printf("Global variable = %d\n", globalVar);
+    printf("Local variable = %d\n", localVar);
+
+    return 0;
+}
